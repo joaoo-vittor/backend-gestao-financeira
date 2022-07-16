29513cc7d70963de50b8a78753bd9990257dd6df
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 15:40:06 -0300

    feat: make migration to table plans_contract of the schema user

---
+++ src/infra/migrations/versions/b948fc87c266_create_table_plans_contract_to_schema_.py
@@ -0,0 +1,51 @@
+"""create table plans_contract to schema user
+
+Revision ID: b948fc87c266
+Revises: 865710ccb03f
+Create Date: 2022-07-09 22:22:22.663573
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql import func
+
+# revision identifiers, used by Alembic.
+revision = "b948fc87c266"
+down_revision = "865710ccb03f"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "plans_contract",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column(
+            "user_id",
+            sa.Integer,
+            sa.ForeignKey(
+                "user.user.id",
+                name="user",
+            ),
+            nullable=False,
+        ),
+        sa.Column(
+            "plan_id",
+            sa.Integer,
+            sa.ForeignKey("user.plan.id", name="user"),
+            nullable=False,
+        ),
+        sa.Column("value_plan", sa.Float, nullable=False),
+        sa.column(
+            "start_time_stamp", sa.DateTime(timezone=True), server_default=func.now()
+        ),
+        sa.Column("end_time_stamp", sa.DateTime(timezone=True)),
+        sa.Column("active", sa.Boolean, default=True),
+        sa.Column("created_at", sa.DateTime(timezone=True), server_default=func.now()),
+        sa.Column("updated_at", sa.DateTime(timezone=True), onupdate=func.now()),
+        schema="user",
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("plans_contract", schema="user")
