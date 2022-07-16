2863145fe3ebad92d0a836664658c6f93d3ff090
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 15:46:31 -0300

    feat: make migration to table expense of the schema expense

---
+++ src/infra/migrations/versions/eaa83c87cb41_create_table_expense_to_schema_expense.py
@@ -0,0 +1,54 @@
+"""create table expense to schema expense
+
+Revision ID: eaa83c87cb41
+Revises: 247678b5e766
+Create Date: 2022-07-09 22:40:42.528661
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql import func
+
+
+# revision identifiers, used by Alembic.
+revision = "eaa83c87cb41"
+down_revision = "247678b5e766"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "expense",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column(
+            "user_id",
+            sa.Integer,
+            sa.ForeignKey("user.user.id", name="user"),
+            nullable=False,
+        ),
+        sa.Column(
+            "category_id",
+            sa.Integer,
+            sa.ForeignKey("expense.category", name="category"),
+            nullable=False,
+        ),
+        sa.Column(
+            "payment_method_id",
+            sa.Integer,
+            sa.ForeignKey("expense.payment_method.id", name="payment_method"),
+            nullable=False,
+        ),
+        sa.Column("name", sa.String(128), nullable=False),
+        sa.Column("value", sa.Float, nullable=False),
+        sa.Column("status", sa.String(128), nullable=False),
+        sa.Column("time_stamp", sa.DateTime(timezone=True), nullable=False),
+        sa.Column("voucher", sa.String(255), nullable=True),
+        sa.Column("created_at", sa.DateTime(timezone=True), server_default=func.now()),
+        sa.Column("updated_at", sa.DateTime(timezone=True), onupdate=func.now()),
+        schema="expense",
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("expense", schema="expense")
