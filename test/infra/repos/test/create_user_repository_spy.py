783de44d2078a9fff50c93aee2974663a5924201
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 19:09:40 -0300

    feat: create CreateUserRepositorySpy

---
+++ test/infra/repos/test/create_user_repository_spy.py
@@ -0,0 +1,24 @@
+class CreateUserRepositorySpy:
+    def __init__(self, connection_handler) -> None:
+        self.__connection_handler = connection_handler
+        self.user_data = None
+
+    def create_user(self, user_data: dict) -> None:
+        """Create user
+
+        Args:
+            user_data (CreateUserModel): model with data to create user
+
+        Returns:
+            UserModel: model with data of the user created
+        """
+
+        if user_data is None:
+            return None
+
+        self.user_data = user_data
+
+        return {
+            "name": user_data if "name" in user_data.keys() else None,
+            "email": user_data["email"],
+        }
