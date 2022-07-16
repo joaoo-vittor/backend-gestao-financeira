69db37e1f2c8f95b3c675727b57fb6ba9dd77388
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 15:41:02 -0300

    feat: make migration to table plan of the schema user

---
+++ src/infra/migrations/versions/865710ccb03f_create_table_plan_to_schema_user.py
@@ -0,0 +1,34 @@
+"""create table plan to schema user
+
+Revision ID: 865710ccb03f
+Revises: 3e32cc5f8dea
+Create Date: 2022-07-09 22:17:34.011417
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql import func
+
+
+# revision identifiers, used by Alembic.
+revision = "865710ccb03f"
+down_revision = "3e32cc5f8dea"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "plan",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("type", sa.String(50), nullable=False),
+        sa.Column("price", sa.Float, default=0.0),
+        sa.Column("active", sa.Boolean, default=False),
+        sa.Column("created_at", sa.DateTime(timezone=True), server_default=func.now()),
+        sa.Column("updated_at", sa.DateTime(timezone=True), onupdate=func.now()),
+        schema="user",
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("plan", schema="user")
