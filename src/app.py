648fe1516f0edcd64d6bcfaaaa063a186b711368
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 18:43:22 -0300

    chore: delete unnecessary files

--- src/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-
-@app.route("/", methods=["GET"])
-def index():
-    return jsonify({"msg": "hello world"}), 200
--- src/test_dummy.py
+++
@@ -1,3 +0,0 @@
-def test_dummy():
-    to_be = True
-    assert to_be or not to_be
