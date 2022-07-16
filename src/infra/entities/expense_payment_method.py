05a6cfee403ccc22bc0e4a6f06d2b702f565cab5
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 15:53:22 -0300

    feat: create model to payment method

---
+++ src/infra/entities/expense_payment_method.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.sql import func
+from src.infra.config import Base
+
+
+class PaymentMethod(Base):
+    """PaymentMethod Entity"""
+
+    __tablename__ = "payment_method"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    def __repr__(self) -> str:
+        return f"PaymentMethod(name={self.name}"
