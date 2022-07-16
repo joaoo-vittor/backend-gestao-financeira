d265d6e3b86445bc769be9cf388d9b8b4af470fa
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 15:53:38 -0300

    feat: create model to category

---
+++ src/infra/entities/expense_category.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, ForeignKey, String, Integer, DateTime
+from sqlalchemy.sql import func
+from src.infra.config import Base
+
+
+class Category(Base):
+    """Category Entity"""
+
+    __tablename__ = "category"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(
+        Integer,
+        ForeignKey(
+            "user.user.id",
+            name="user",
+        ),
+        nullable=False,
+    )
+    name = Column(String(128), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    def __repr__(self) -> str:
+        return f"Category(name={self.name}"
