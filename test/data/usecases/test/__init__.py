a2a1adf3fc2027192b4665e8cff2fd869ea9079c
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 20:27:05 -0300

    feat: implemeting CreateUserUseCaseSpy

---
+++ test/data/usecases/test/__init__.py
@@ -0,0 +1,1 @@
+from .create_user_spy import CreateUserUseCaseSpy
---
+++ test/data/usecases/test/create_user_spy.py
@@ -0,0 +1,15 @@
+from typing import Any
+from src.domain.models import UserModel
+
+
+class CreateUserUseCaseSpy:
+    def __init__(self, create_user_repository: Any, encrypter: Any) -> None:
+        self.__encrypter = encrypter
+        self.__create_user_repository = create_user_repository
+        self.user = None
+
+    def add(self, user: Any) -> Any:
+        self.user = user
+        return UserModel(
+            name=user["name"] if "name" in user.keys() else None, email=user["email"]
+        )
