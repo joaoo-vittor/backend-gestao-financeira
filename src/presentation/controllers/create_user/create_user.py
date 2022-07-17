03cfb590997b60cbdbc1598ee94cc2c9551da2ca
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 23:07:26 -0300

    feat: ensure CreateUserController returns valid http response if http request is valid

---
+++ src/presentation/controllers/create_user/create_user.py
@@ -0,0 +1,63 @@
+from typing import Type
+from src.main.interface import HandlerInterface
+from src.domain.usecases import CreateUserInterface as CreateUser
+from src.presentation.helpers import HttpRequest, HttpResponse
+from src.presentation.interface import Validator
+from src.presentation.errors import HttpErrors
+from src.domain.usecases import CreateUserModel
+
+
+class CreateUserController(HandlerInterface):
+    def __init__(self, create_user_usecase: CreateUser, validator: Validator) -> None:
+        self.__create_user_usecase = create_user_usecase
+        self.__validator = validator
+
+    def handler(self, http_request: Type[HttpRequest]) -> HttpResponse:
+        """Handler to call on route
+
+        Args:
+            http_request (Type[HttpRequest]): http request with data
+
+        Returns:
+            HttpResponse: response data
+        """
+
+        response = None
+
+        if http_request.body:
+            try:
+                body_is_valid = self.__validator.validate(http_request.body)
+
+                if body_is_valid:
+                    user = CreateUserModel(**http_request.body)
+                    response = self.__create_user_usecase.add(user)
+                    return HttpResponse(
+                        status_code=200,
+                        body={
+                            "data": {
+                                "type": "user",
+                                "user": {
+                                    "name": response["name"],
+                                    "email": response["email"],
+                                },
+                            }
+                        },
+                    )
+
+                http_error = HttpErrors.error_422()
+                return HttpResponse(
+                    status_code=http_error["status_code"],
+                    body=http_error["body"],
+                )
+
+            except Exception:
+                http_error = HttpErrors.error_500()
+                return HttpResponse(
+                    status_code=http_error["status_code"],
+                    body=http_error["body"],
+                )
+
+        http_error = HttpErrors.error_400()
+        return HttpResponse(
+            status_code=http_error["status_code"], body=http_error["body"]
+        )
