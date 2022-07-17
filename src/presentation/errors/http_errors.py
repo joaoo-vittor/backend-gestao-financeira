8745858a15ec1a3561da35ef627975501b9e42de
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 23:06:10 -0300

    chore: chage format data

--- src/presentation/errors/http_errors.py
+++ src/presentation/errors/http_errors.py
@@ -5,22 +5,26 @@ class HttpErrors:
     def error_422():
         """HTTP 422"""
 
-        return {"status_code": 422, "body": {"error": "Unprecessable Entity"}}
+        return {
+            "errors": {"status_code": 422, "body": {"error": "Unprecessable Entity"}}
+        }
 
     @staticmethod
     def error_400():
         """HTTP 400"""
 
-        return {"status_code": 400, "body": {"error": "Bad Request"}}
+        return {"errors": {"status_code": 400, "body": {"error": "Bad Request"}}}
 
     @staticmethod
     def error_409():
         """HTTP 409"""
 
-        return {"status_code": 409, "body": {"error": "Conflict"}}
+        return {"errors": {"status_code": 409, "body": {"error": "Conflict"}}}
 
     @staticmethod
     def error_500():
         """HTTP 409"""
 
-        return {"status_code": 500, "body": {"error": "Internal Server Error"}}
+        return {
+            "errors": {"status_code": 500, "body": {"error": "Internal Server Error"}}
+        }
