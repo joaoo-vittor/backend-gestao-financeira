6e7a27506e1f8c03dc732420b68d175d4a093fea
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-17 18:54:16 -0300

    fix: return type

--- src/data/interfaces/login_user_repository.py
+++ src/data/interfaces/login_user_repository.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import Union
 from src.domain.usecases.user import LoginUserModel
-from src.domain.models.user import User
+from src.domain.models.user import UserLogin
 
 
 class LoginUserRespositoryInterface(ABC):
     @abstractmethod
-    def find_user(self, user_data: LoginUserModel) -> Union[User, None]:
+    def find_user(self, user_data: LoginUserModel) -> Union[UserLogin, None]:
         raise Exception("Implementing method: find_user")
