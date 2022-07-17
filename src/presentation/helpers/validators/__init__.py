717defec032e5013ec1ad1b01286cf50db343115
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 23:44:26 -0300

    feat: implementing validate CreateUser body

---
+++ src/presentation/helpers/validators/__init__.py
@@ -0,0 +1,1 @@
+from .validator_create_user import ValidatorBodyCreateUser
---
+++ src/presentation/helpers/validators/validator_create_user.py
@@ -0,0 +1,17 @@
+from cerberus import Validator
+from src.presentation.interface import Validator as ValidatorInterface
+
+
+class ValidatorBodyCreateUser(ValidatorInterface):
+    def validate(self, data: dict) -> bool:
+        schema = {
+            "name": {"type": "string", "required": True},
+            "email": {
+                "type": "string",
+                "regex": "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+                "required": True,
+            },
+            "password": {"type": "string", "required": True},
+        }
+        validator = Validator(schema)
+        return validator.validate(data)
