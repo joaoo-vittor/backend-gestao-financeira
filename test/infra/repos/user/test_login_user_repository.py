3f0a78dabb7f22594c41b4fa1c30b012382ae746
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-17 17:03:26 -0300

    test: ensure LoginUserRepository return user if call find user with valid data

--- test/infra/repos/user/test_login_user_repository.py
+++ test/infra/repos/user/test_login_user_repository.py
@@ -1,7 +1,8 @@
 from faker import Faker
+import pytest
 from src.infra.repos.user import LoginUserRepository
 from src.infra.config import DBConnectionHandler
-
+from src.domain.usecases.user import FindUserModel
 
 faker = Faker()
 
@@ -15,8 +16,29 @@ def make_sut() -> LoginUserRepository:
     return LoginUserRepository(connection_handler)
 
 
+@pytest.fixture
+def drop_database():
+    connection_handler = DBConnectionHandler(CONNECTION_STRING_TEST)
+    engine = connection_handler.get_engine()
+    engine.execute(
+        "INSERT INTO users.users(id, email, password_hash)\
+        VALUES ('1', 'any_email@email.com', 'any_password')"
+    )
+    yield
+    engine.execute("DELETE FROM users.users")
+
+
 def test_should_return_none_if_pass_none_to_find_user():
     sut = make_sut()
     response = sut.find_user(None)
 
     assert response is None
+
+
+def test_should_return_user_model_if_call_find_user_with_valid_values(drop_database):
+    sut = make_sut()
+    email = "any_email@email.com"
+    user = FindUserModel(email=email, password="any_password")
+    response = sut.find_user(user)
+
+    assert response["email"] == email
