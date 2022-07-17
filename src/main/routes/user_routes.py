16060d056271eb18ca5d3f788b5d6159000f9fa5
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-17 01:28:27 -0300

    feat: implemeting route to create user

---
+++ src/main/routes/user_routes.py
@@ -0,0 +1,12 @@
+from flask import jsonify, request, Blueprint
+from src.main.adapter import flask_adapter
+from src.main.composers import create_user_compose
+
+
+user_routes_bp = Blueprint("user_routes_bp", __name__)
+
+
+@user_routes_bp.route("/user/register", methods=["POST"])
+def register_user():
+    response = flask_adapter(request, create_user_compose().handler)
+    return jsonify(response.body), response.status_code
