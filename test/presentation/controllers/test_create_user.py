9c551b638f7729da2b755e15174a67289508be47
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-16 23:15:04 -0300

    test: ensure CreateUserController return 400 if http request is None

--- test/presentation/controllers/test_create_user.py
+++ test/presentation/controllers/test_create_user.py
@@ -51,9 +51,14 @@ def make_http_request():
 def test_should_return_status_200_and_data_if_http_request_is_valid():
     sut = make_sut().sut
     http_request = make_http_request()
-
     response = sut.handler(http_request)
 
     assert response.status_code == 200
     assert response.body["data"]["user"]["name"] == http_request.body["name"]
     assert response.body["data"]["user"]["email"] == http_request.body["email"]
+
+
+def test_should_return_status_400_if_body_is_none():
+    sut = make_sut().sut
+    response = sut.handler(None)
+    assert response.status_code == 400
