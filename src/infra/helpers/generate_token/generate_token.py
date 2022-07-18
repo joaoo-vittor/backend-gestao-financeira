5aa6cdb4854baaaf1318eba9d2e6705655ba350f
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-17 23:11:29 -0300

    feat: implementing GenerateToken

---
+++ src/infra/helpers/generate_token/generate_token.py
@@ -0,0 +1,11 @@
+import datetime
+from src.data.interfaces import GenerateToken
+from flask_jwt_extended import create_access_token
+
+
+class GenerateToken(GenerateToken):
+    def generate_token(self, payload: dict) -> str:
+        return create_access_token(
+            identity=payload,
+            expires_delta=datetime.datetime.now() + datetime.timedelta(days=7),
+        )
