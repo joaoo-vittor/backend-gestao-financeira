d58f20fccc9d5fa961871d9fba67649cbcda874b
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-17 22:05:59 -0300

    chore: add Faker to return fake data

--- test/infra/repos/test/login_user_repository_spy.py
+++ test/infra/repos/test/login_user_repository_spy.py
@@ -1,13 +1,33 @@
 from typing import Any, Union
-from src.domain.models.user import User
+from src.domain.models.user import UserLogin, Plan, PlanContract
+from faker import Faker
+
+faker = Faker()
 
 
-class LoginUserRepository:
+class LoginUserRepositorySpy:
     def __init__(self, connection_handler: Any) -> None:
         self.__connection_handler = connection_handler
         self.user_data = None
 
-    def find_user(self, user_data: Any) -> Union[User, None]:
+    def find_user(self, user_data: Any) -> Union[UserLogin, None]:
         self.user_data = user_data
 
-        return User(email=user_data["email"], password_hash=user_data["password"])
+        user_id = faker.random_digit_not_null()
+        plan = Plan(type="Freemium", active=True)
+        plan_contract = PlanContract(
+            user_id=user_id,
+            plan_id=faker.random_digit_not_null(),
+            value_plan=faker.random_number(digits=3),
+            start_time_stamp=faker.date(),
+            end_time_stamp=faker.date(),
+            active=True,
+        )
+
+        return UserLogin(
+            id=user_id,
+            email=user_data["email"],
+            password_hash=user_data["password"],
+            plan_contract=plan_contract,
+            plan=plan,
+        )
