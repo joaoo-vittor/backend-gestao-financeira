71081343513fb0ef497d57bf6dfb15f45a18ef67
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-17 23:33:49 -0300

    feat: create route to login

--- src/main/routes/user_routes.py
+++ src/main/routes/user_routes.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request, Blueprint
 from src.main.adapter import flask_adapter
-from src.main.composers import create_user_compose
+from src.main.composers import create_user_compose, login_user_compose
 
 
 user_routes_bp = Blueprint("user_routes_bp", __name__)
@@ -9,4 +9,10 @@ user_routes_bp = Blueprint("user_routes_bp", __name__)
 @user_routes_bp.route("/user/register", methods=["POST"])
 def register_user():
     response = flask_adapter(request, create_user_compose().handler)
+    return jsonify(response.body), response.status_code
+
+
+@user_routes_bp.route("/user/login", methods=["POST"])
+def login_user():
+    response = flask_adapter(request, login_user_compose().handler)
     return jsonify(response.body), response.status_code
