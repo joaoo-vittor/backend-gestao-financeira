65d3b19716a0a865de4b930b2ac6844d5afb7dca
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-17 23:32:09 -0300

    fix: remove datetime now

--- src/infra/helpers/generate_token/generate_token.py
+++ src/infra/helpers/generate_token/generate_token.py
@@ -7,5 +7,5 @@ class GenerateToken(GenerateToken):
     def generate_token(self, payload: dict) -> str:
         return create_access_token(
             identity=payload,
-            expires_delta=datetime.datetime.now() + datetime.timedelta(days=7),
+            expires_delta=datetime.timedelta(days=7),
         )
