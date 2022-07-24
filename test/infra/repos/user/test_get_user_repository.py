d2f1251a2a39e39ce905ce85593e7e62283d7c22
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-23 22:36:59 -0300

    reafctor: move insert user to method helper

--- test/infra/repos/user/test_get_user_repository.py
+++ test/infra/repos/user/test_get_user_repository.py
@@ -24,36 +24,37 @@ def drop_database():
     engine.execute("DELETE FROM users.users")
 
 
-def make_user_data() -> dict:
-    return {
+def make_user_data(drop_database) -> dict:
+    data_user = {
         "id": faker.random_number(digits=2),
         "password_hash": faker.password(),
         "email": faker.email(),
     }
 
-
-def test_should_return_user_data_if_email_and_id_is_correct(drop_database):
-    user_data = make_user_data()
-
-    sut = make_sut()
-
     insert_user_query = "INSERT INTO\
         users.users(id, email, password_hash)\
         VALUES ('{id}','{email}','{password_hash}')".format(
-        **user_data
+        **data_user
     )
     plans_contract_query = "INSERT INTO\
         users.plans_contract(id, user_id, plan_id, value_plan,\
             start_time_stamp, end_time_stamp)\
         VALUES ('1', '{id}', '1', '0.0',\
         current_date, current_date + INTERVAL '2 day')".format(
-        id=user_data["id"]
+        id=data_user["id"]
     )
+
     drop_database.execute(insert_user_query)
     drop_database.execute(plans_contract_query)
 
-    user_params = GetUserParams(id=user_data["id"], email=user_data["email"])
+    return data_user
 
+
+def test_should_return_user_data_if_email_and_id_is_correct(drop_database):
+    user_data = make_user_data(drop_database)
+    sut = make_sut()
+
+    user_params = GetUserParams(id=user_data["id"], email=user_data["email"])
     response = sut.find_user_by(user_params)
 
     assert response["id"] == user_data["id"]
