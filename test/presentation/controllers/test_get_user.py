0c55b405e189d4e186bb5bd951e5f06678d21c47
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-24 00:24:42 -0300

    test: ensure GetUserController return status code 500 if raise exception

--- test/presentation/controllers/test_get_user.py
+++ test/presentation/controllers/test_get_user.py
@@ -17,6 +17,11 @@ class ValidateHeaderGetUserSpy(Validator):
         return self.is_valid
 
 
+class ValidateHeaderGetUserSpyWithRaise(Validator):
+    def validate(self, data: dict) -> bool:
+        raise Exception("")
+
+
 class DecryptSpy(Decrypt):
     def __init__(self) -> None:
         self.payload = {"id": faker.random_number(), "email": faker.email()}
@@ -36,6 +41,15 @@ def make_sut() -> SutTypes:
     repo = GetUserRepositorySpy(None)
     use_case = GetUserUseCaseSpy(repo)
     validator = ValidateHeaderGetUserSpy()
+    decrypt = DecryptSpy()
+    sut = GetUserController(use_case, validator, decrypt)
+    return SutTypes(sut=sut, validator=validator, decrypt=decrypt)
+
+
+def make_sut_with_validator_exception() -> SutTypes:
+    repo = GetUserRepositorySpy(None)
+    use_case = GetUserUseCaseSpy(repo)
+    validator = ValidateHeaderGetUserSpyWithRaise()
     decrypt = DecryptSpy()
     sut = GetUserController(use_case, validator, decrypt)
     return SutTypes(sut=sut, validator=validator, decrypt=decrypt)
@@ -78,4 +92,16 @@ def test_should_return_status_401_if_authentication_token_in_header_is_invalid()
     http_response = sut.handler(http_request)
 
     assert http_response.status_code == 401
+    assert "error" in http_response.body["errors"]["body"].keys()
+
+
+def test_should_return_status_500_if_raise_exception():
+    sut = make_sut_with_validator_exception().sut
+
+    header = {"Authentication": "any_token"}
+    http_request = HttpRequest(header=header)
+
+    http_response = sut.handler(http_request)
+
+    assert http_response.status_code == 500
     assert "error" in http_response.body["errors"]["body"].keys()
