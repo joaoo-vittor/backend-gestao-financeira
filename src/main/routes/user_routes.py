bf71a875b708cec93d1c3a4053ac254e3cfab00e
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-24 01:44:54 -0300

    feat: create route to get user data

--- src/main/routes/user_routes.py
+++ src/main/routes/user_routes.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request, Blueprint
 from src.main.adapter import flask_adapter
-from src.main.composers import create_user_compose, login_user_compose
-
+from src.main.composers import create_user_compose, login_user_compose, get_user_compose
+from src.main.decorators import tokens_is_required
 
 user_routes_bp = Blueprint("user_routes_bp", __name__)
 
@@ -15,4 +15,11 @@ def register_user():
 @user_routes_bp.route("/user/login", methods=["POST"])
 def login_user():
     response = flask_adapter(request, login_user_compose().handler)
+    return jsonify(response.body), response.status_code
+
+
+@user_routes_bp.route("/user", methods=["GET"])
+@tokens_is_required
+def get_user():
+    response = flask_adapter(request, get_user_compose().handler)
     return jsonify(response.body), response.status_code
