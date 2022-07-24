5c1db8fa242fe86cc09df93a8561b5922b5fd1cc
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-07-24 01:41:27 -0300

    chore: improve DecryptToken to get expire token and payload

--- src/infra/helpers/dencrypt/dencrypt.py
+++ src/infra/helpers/dencrypt/dencrypt.py
@@ -1,7 +1,13 @@
+from datetime import datetime, timezone
 from src.presentation.interface import Decrypt
 from flask_jwt_extended import decode_token
 
 
 class DecryptToken(Decrypt):
     def dencrypt(self, token: str) -> dict:
-        return decode_token(token)
+        data_token = decode_token(token)
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now)
+        if target_timestamp > data_token["exp"]:
+            return {}
+        return data_token["sub"]
